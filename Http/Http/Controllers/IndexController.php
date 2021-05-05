a1230907e669a91711b6c5bcf3c680b1e8926cf1
Santosh Kumar Potu <spotu@pkrmsezdc.com>
2021-05-06 00:08:08 +0530

    fix

--- Http/Http/Controllers/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Http\Controllers;
-
-class IndexController extends BaseController {
-    
-}
--- views/405.php
+++ views/405.php
@@ -1,5 +1,5 @@
 <?php
-echo "method not allowed";
+echo "Method not allowed";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
